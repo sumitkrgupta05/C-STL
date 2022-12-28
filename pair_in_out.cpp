1f44c924b622be193fcdfdfd754dced39dd2c154
Sumit Kumar Gupta <90912779+sumitkrgupta05@users.noreply.github.com>
2022-12-28 12:19:27 +0530

    Add files via upload
    basic pair program

---
+++ pair_in_out.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    // ******************  SIMPLE PAIR   *******************
+    /*
+        pair<int, int> p = {1, 5};
+        cout << p.first << endl
+            << p.second;
+
+OR
+
+        pair<int, string> str;
+        str = {5,"sumit"};
+        str = make_pair(5, "sumit");
+        cout << str.first << " " << str.second << endl;
+
+    */
+
+    // ***************  NESTED pair    ******************
+    /*
+            pair<int, pair<int, int>> p = {1, {5, 3}};
+            cout << p.first << endl
+                << p.second.first << endl
+                << p.second.second;
+     */
+
+    // **************   PAIR ARRAY    *************************
+    /*
+        pair<int, int> arr[] = {{1, 7}, {5, 3}, {11, 12}};
+        cout << arr[0].first << "             " << arr[0].second << endl;
+        cout << arr[1].first << "             " << arr[1].second << endl;
+        cout << arr[2].first << "             " << arr[2].second << endl;
+    */
+    return 0;
+}
